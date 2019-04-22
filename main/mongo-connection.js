9b7b309feee6f31b4eb9729cdb9144377f719fdb
Joe Constantine <jconstan@bu.edu>
2019-04-22 17:40:48 -0400

    mongo on cloud now

--- main/mongo-connection.js
+++ main/mongo-connection.js
@@ -1,12 +1,18 @@
 var mongo = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
-
+var mpswd = require('./mongopswd.js').pswd;
 const assert = require('assert');
+console.log("mpswd: " + mpswd);
 
-const url = 'mongodb://localhost:27017';
-const dbName = 'myproject';
- 
+
+//const uri = "mongodb+srv://joe:" + String(mpswd) + "@cluster0-rya5t.gcp.mongodb.net/test?retryWrites=true";
+//const uri = "mongodb+srv://joe:mypassword@cluster0-rya5t.gcp.mongodb.net/test?retryWrites=true";
+var uri = "mongodb://joe:" + mpswd + "@cluster0-shard-00-00-rya5t.gcp.mongodb.net:27017,cluster0-shard-00-01-rya5t.gcp.mongodb.net:27017,cluster0-shard-00-02-rya5t.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+
+
 var _db;
+
+
 
 
 //export the db instance for use in other files
@@ -14,11 +20,31 @@ module.exports = {
 
   //function to connect to the db
   connectToServer: function( callback ) {
-    MongoClient.connect( "mongodb://localhost:27017/myproject", function( err, db ) {
-      _db = db;
-      return callback( err );
-    } );
+    MongoClient.connect(uri, function(err, client) {
+      const collection = client.db("SmartPlanner").collection("users");
+      // perform actions on the collection object
+      _db = client.db("SmartPlanner");
+      return callback(err);
+    });
+    
+      
+      
+      
+    /*const client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+      const collection = client.db("SmartPlanner").collection("users");
+      _db = client.db("SmartPlanner");
+      if (err) throw err;
+      return callback(err);
+    });*/
+    
   },
+
+   //MongoClient.connect( "mongodb://localhost:27017/SiteBlocker", function( err, db ) {
+    //  _db = db;
+    //  return callback( err );
+    //} );
+  //},
 
   //db getter
   getDb: function() {
