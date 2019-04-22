6c2b3599a59dd8a20f99749480441297cdc1f783
Yuxin Sun <yxsun@bu.edu>
2019-04-22 16:51:41 -0400

    changes on google login

--- main/routes/googlelogin.js
+++
@@ -1,7 +0,0 @@
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  c$(".g-signin2").css("display", "none");
-  $(".data").css("display", "block");
-  $("#pic").attr("src", profile.getImageUrl());
-  $("#email").text(profile.getEmail());
-}
