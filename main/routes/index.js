20432639aeda63e817bfcb780f440de63caa8fa9
Jessy <jinshuy@bu.edu>
2019-04-23 18:48:27 -0400

    update UI

--- main/routes/index.js
+++ main/routes/index.js
@@ -424,7 +424,9 @@ router.post('/focus', function(req, res, next){
     res.render('focus');
 });
 
-
+router.post('/getdata', function(req, res, next){
+    res.render('blockedSites');
+});
 
 
 module.exports = router;
